c6ae338f3ab750526f60d902223b14778d0e531e
Sakshi Khoobchandani <sakshikbc@gmail.com>
2021-10-21 12:46:59 +0530

    Unique Email

--- Core/Database.php
+++ Core/Database.php
@@ -88,4 +88,10 @@ class Database
         echo '['.date('Y-m-d H:i:s').'] - '. $message.PHP_EOL;
     }
 
+    public function prepare($sql)
+    {
+        return $this->pdo->prepare($sql);
+        # code...
+    }
+
 }
--- Core/Model.php
+++ Core/Model.php
@@ -9,6 +9,7 @@ abstract class Model
     public const RULE_MIN = 'min';
     public const RULE_MAX = 'max';
     public const RULE_MATCH = 'match';
+    public const RULE_UNIQUE = 'unique';
 
     public function loadData($data)
     {
@@ -51,6 +52,19 @@ abstract class Model
                 if($ruleName === self::RULE_MATCH && $value !== $this->{$rule['match']}) {
                     $this->addError($attribute, self::RULE_MATCH, $rule);
                 }
+
+                if($ruleName === self::RULE_UNIQUE) {
+                    $className = $rule['class'];
+                    $unique_attribute = $rule['attribute'] ?? $attribute;
+                    $tableName = $className::tableName();
+                    $statement = Application::$app->db->prepare("SELECT * FROM $tableName WHERE $unique_attribute = :$unique_attribute");
+                    $statement->bindValue(":$unique_attribute", $value);
+                    $statement->execute();
+                    $record = $statement->fetchObject();
+                    if($record) {
+                        $this->addError($attribute, self::RULE_UNIQUE, ['field' => $attribute]);
+                    }
+                }
             }
         }
         return empty($this->errors);
@@ -74,7 +88,8 @@ abstract class Model
             self::RULE_EMAIL => "This field must be a valid Email address",
             self::RULE_MIN => "Min length of this field must be {min}",
             self::RULE_MAX => "Max length of this field must be {max}",
-            self::RULE_MATCH => "This field must be the same as {match}"
+            self::RULE_MATCH => "This field must be the same as {match}",
+            self::RULE_UNIQUE => "Record with this {field} already exists "
         ];
     }
 
